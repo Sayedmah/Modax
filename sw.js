185c92d67337273822a0cea736daf7021c8fbf4a
Sayedmah <117022771+Sayedmah@users.noreply.github.com>
2026-09-17 14:39:54 +0300

    Refresh MODY chat answer and sources UI

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mody-ai-web-v5';
+const CACHE='mody-ai-web-v6';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./mody-icon.svg'];
 const MODY_BACKEND='https://modaxai.onrender.com';
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
